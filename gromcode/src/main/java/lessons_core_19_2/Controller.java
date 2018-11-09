17d01d84469c87344637558d0c8c37d503d6df9f
kaluzhniy <kaluzhniy@192.168.5.50>
2018-11-09 17:07:51 +0200

    lessons_core_19_2 Corrected Controller-class

--- gromcode/src/main/java/lessons_core_19_2/Controller.java
+++ gromcode/src/main/java/lessons_core_19_2/Controller.java
@@ -5,8 +5,22 @@ public class Controller {
 	
 	public File put(Storage storage, File file) throws Exception {
 		if (ValidatorInputData.isInputDataCorrect(storage, file)) {
-			storage.setFiles(addToFileArray(storage.getFiles(), file));
-			return file;
+			File[] fileArray = storage.getFiles();
+			if (fileArray == null) {
+				fileArray = new File[startNumberElementArray];
+				fileArray[0] = file;
+				storage.setFiles(fileArray);
+				return file;
+			} else {
+				for (int i = 0; i < fileArray.length; ++i) {
+					if (fileArray[i] == null) {
+						fileArray[i] = file;
+						storage.setFiles(fileArray);
+						return file;
+					}
+				}
+				throw new Exception("Storage is full");
+			}
 		} 
 		throw new Exception("not put File with ID = " + file.getId() 
 			+ " in to Storage with ID = " + storage.getId() + "\n");
@@ -14,8 +28,14 @@ public class Controller {
 
 	public File delete(Storage storage, File file) throws Exception {
 		if (ValidatorInputData.isIdInStorage(storage, file)) {
-			storage.setFiles(removeFileFromArray(storage.getFiles(), file));
-			return file;
+			File[] fileArray = storage.getFiles();
+			for (int i = 0; i < fileArray.length; ++i) {
+				if (fileArray[i] == file) {
+					fileArray[i] = null;
+					storage.setFiles(fileArray);
+					return file;
+				}
+			}
 		} 
 		throw new Exception("not delete File with ID = " + file.getId() 
 			+ " from Storage with ID = " + storage.getId() + "\n");
@@ -25,8 +45,8 @@ public class Controller {
 		for (File file : storageFrom.getFiles()) {
 			if (file != null) {
 				if (ValidatorInputData.isInputDataCorrect(storageTo, file)) {
-					storageTo.setFiles(addToFileArray(storageTo.getFiles(), file));
-					storageFrom.setFiles(removeFileFromArray(storageFrom.getFiles(), file));
+					put(storageTo, file);
+					delete(storageFrom, file);
 				} else {
 					throw new Exception("not transfer all files from Storage with ID = " + storageFrom.getId() 
 						+ " to Storage with ID = " + storageTo.getId() + "\n");
@@ -38,41 +58,15 @@ public class Controller {
 	public void transferFile(Storage storageFrom, Storage storageTo, long id) throws Exception {
 		File file = findFileInStorage(storageFrom, id); 
 		if (ValidatorInputData.isInputDataCorrect(storageTo, file)) {
-			storageTo.setFiles(addToFileArray(storageTo.getFiles(), file));
-			storageFrom.setFiles(removeFileFromArray(storageFrom.getFiles(), file));
+			put(storageTo, file);
+			delete(storageFrom, file);
 		} else {
 			throw new Exception("not transfer File with ID = " + id 
 					+ " in to Storage with ID = " + storageTo.getId()
 					+ " from Storage with ID = " + storageFrom.getId() + "\n");
 		}
 	}
-	
-	private File[] addToFileArray(File[] fileArray, File newFile) throws Exception {
-		if (fileArray == null) {
-			fileArray = new File[startNumberElementArray];
-			fileArray[0] = newFile;
-			return fileArray;
-		} else {
-			for (int i = 0; i < fileArray.length; ++i) {
-				if (fileArray[i] == null) {
-					fileArray[i] = newFile;
-					return fileArray;
-				}
-			}
-			throw new Exception("Storage is full");
-		}
-	}
-	
-	private File[] removeFileFromArray(File[] fileArray, File removeFile) {
-		for (int i = 0; i < fileArray.length; ++i) {
-			if (fileArray[i] == removeFile) {
-				fileArray[i] = null;
-				return fileArray;
-			}
-		}
-		return null;
-	}
-	
+		
 	private File findFileInStorage(Storage storage, long id) throws Exception {
 		for (File files : storage.getFiles()) {
 			if (files != null && files.getId() == id) {
